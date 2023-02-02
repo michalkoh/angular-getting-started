fe1c6982875a6b033697a0c613131831010b0c45
michal.kohut <michalkoh@gmail.com>
2023-02-02 08:42:13 +0100

    Data Binding & Pipes

--- src/products/product-list.component.ts
+++ src/products/product-list.component.ts
@@ -7,4 +7,34 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "18.03.2021",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "21.05.2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+        }
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
